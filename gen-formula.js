668845502de9130340f623bd9d7f750f23e85cb3
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2022-12-23 18:43:22 +0800

    add ingredient to formulas as numbers

--- gen-formula.js
+++ gen-formula.js
@@ -1,25 +1,19 @@
-import {fromObj,nodefs,readTextLines, writeChanged} from 'ptk/nodebundle.cjs'
+import {alphabetically0, fromObj,nodefs,readTextLines, writeChanged} from 'ptk/nodebundle.cjs'
 await nodefs;
 
-const Ingredient=readTextLines('off/6-ingredient.tsv');
-const rawlines=readTextLines('off/3pujifang.off');
+const Ingredient=readTextLines('off/2-ingredient.tsv');
+const rawlines=readTextLines('off/0pujifang.off');
 const Lexicon_ingredient={};
 const formula_ingredient={};
 const ingredient_formula={};
 const build_ingredient_lemma_key=()=>{
     for (let i=1;i<Ingredient.length;i++) {//skip header
-        const [key, alias]=Ingredient[i].split('\t');
-        Lexicon_ingredient[key]=key;
-        if (alias) {
-            const lemmas=alias.split(',');
-            for (let j=0;j<lemmas.length;j++) {
-                Lexicon_ingredient[lemmas[j]]=key;
-            }
-        }
+        const [key,master]=Ingredient[i].split('\t');
+        Lexicon_ingredient[key]=master||'';
     }
 }
+
 build_ingredient_lemma_key();
-// console.log(Lexicon_ingredient)
 
 let correct=0,error=0;
 
@@ -31,9 +25,10 @@ const parseIngredient=(linetext, formula)=>{
 
     for (let i=0;i<ingredients.length;i++) {
         const ing=ingredients[i];
-        const ingkey=Lexicon_ingredient[ing];
+        let   ingkey=Lexicon_ingredient[ing] ;
+        if (Lexicon_ingredient.hasOwnProperty(ing) && !ingkey) ingkey=ing;
         if (ingkey) {
-            if (!formula_ingredient[formula]) formula_ingredient[formula]=[];
+            
             if (!~formula_ingredient[formula].indexOf(ingkey)){
                 formula_ingredient[formula].push(ingkey);
             }
@@ -58,12 +53,17 @@ const parse_formula=()=>{
         const line=rawlines[i];
         const m=line.match(/formula(\d+)/);
         if (m) {
-            formula=parseInt(m[1])
+            formula=parseInt(m[1]);
+            if (!formula_ingredient[formula]) formula_ingredient[formula]=[];
+            else {
+                console.log('repeated formula',m[1])
+            }
         }
+
         const at=line.indexOf('^ingredient');
         if (!~at) continue;
 
-        parseIngredient(line, formula);
+        formula&&parseIngredient(line, formula);
     }
 }
 
@@ -71,18 +71,17 @@ parse_formula();
 console.log('correct',correct,'error',error, 'rate',(correct-error)/correct)
 
 const gen_formula_ingredient=()=>{
-    const arr=fromObj(formula_ingredient,(a,b)=>a+'\t'+b.join(','));
-
-    arr.unshift('^:<name=formula preload=true caption=方到藥>	name:string alias:string');
+    const arr=fromObj(formula_ingredient,(a,b)=>b.join(','));
+//serial is one-base
+    arr.unshift('^:<name=formula keytype=serial preload=true caption=方劑>ingredients=keys:ingredient');
     writeChanged('off/7-formula.tsv',arr.join('\n'),true);
-  
 }
 
 
 const fill_ingredient_formula=()=>{
     const arr=fromObj(ingredient_formula,(a,b)=>a+'\t'+b.join(','));
-
-    arr.unshift('^:<name=ingredient preload=true caption=藥到方>	name:string alias:string');
+    arr.sort(alphabetically0);
+    arr.unshift('^:<name=ingredient2formula preload=true caption=藥到方>name:string\tformulas=numbers');
     writeChanged('off/8-ingredient.tsv',arr.join('\n'),true);
   
 }
