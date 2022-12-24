08d1318bb770b4381276b5dd9e4a4222024349a5
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2022-12-24 19:19:42 +0800

    change format

--- gen-formula.js
+++ gen-formula.js
@@ -1,11 +1,14 @@
 import {alphabetically0, fromObj,nodefs,readTextLines, writeChanged} from 'ptk/nodebundle.cjs'
 await nodefs;
+//藥 medicine ，又名
+//ingredients ，配方，藥名清單
+//ingredient  ，藥可組成的配方清單
 
-const Ingredient=readTextLines('off/2-ingredient.tsv');
+const Ingredient=readTextLines('off/2-medicine.tsv');
 const rawlines=readTextLines('off/0pujifang.off');
 const Lexicon_ingredient={};
-const formula_ingredient={};
-const ingredient_formula={};
+const ingredients=[];
+const ingredient_members={};
 const build_ingredient_lemma_key=()=>{
     for (let i=1;i<Ingredient.length;i++) {//skip header
         const [key,master]=Ingredient[i].split('\t');
@@ -17,53 +20,38 @@ build_ingredient_lemma_key();
 
 let correct=0,error=0;
 
-const parseIngredient=(linetext, formula)=>{
-    let text=linetext.replace(/（[^）]+）/g,'')
-    .replace(/［[^］]+］/g,'').replace('^ingredient','').replace(/<[^>]+>/g,'')
+const parseIngredient=(linetext, seq,line)=>{
+    let text=linetext.replace(/（[^）]+）/g,' ').replace(/、/g,' ')
+    .replace(/［[^］]+］/g,'').replace(/\^ingredients[\d+ ]*/,'').replace(/<[^>]+>/g,'')
+    const members=text.split(/[　 。]/).filter(it=>!!it);
+    ingredients[seq]=[];
+    for (let i=0;i<members.length;i++) {
+        let mem=members[i]; //一味藥
+        let norm=Lexicon_ingredient[mem] ||mem; 
+        // if (norm!==mem)console.log(mem,norm,line)
 
-    const ingredients=text.split(/[　 。]/).filter(it=>!!it);
-
-    for (let i=0;i<ingredients.length;i++) {
-        const ing=ingredients[i];
-        let   ingkey=Lexicon_ingredient[ing] ;
-        if (Lexicon_ingredient.hasOwnProperty(ing) && !ingkey) ingkey=ing;
-        if (ingkey) {
-            
-            if (!~formula_ingredient[formula].indexOf(ingkey)){
-                formula_ingredient[formula].push(ingkey);
-            }
-
-            if (!ingredient_formula[ingkey]) ingredient_formula[ingkey] = [];
-
-            if (!~ingredient_formula[ingkey].indexOf(formula)){
-                ingredient_formula[ingkey].push(formula);
-            }
-
-                
+        if (Lexicon_ingredient.hasOwnProperty(mem)) {
+            ingredients[seq].push(norm); //用正規名
             correct++;
+
+            if (!ingredient_members[norm])ingredient_members[norm]=[];
+            ingredient_members[norm].push(seq)
         } else {    
             error++
-            // console.log(formula,ing)
+            if (mem.length>8) console.log(seq+1,mem)
         }
+
+
     }
+    ingredients[seq].sort();
 }
 const parse_formula=()=>{
-    let formula=0;
     for (let i=0;i<rawlines.length;i++) {
         const line=rawlines[i];
-        const m=line.match(/formula(\d+)/);
-        if (m) {
-            formula=parseInt(m[1]);
-            if (!formula_ingredient[formula]) formula_ingredient[formula]=[];
-            else {
-                console.log('repeated formula',m[1])
-            }
-        }
-
-        const at=line.indexOf('^ingredient');
-        if (!~at) continue;
-
-        formula&&parseIngredient(line, formula);
+        const m=line.match(/ingredients(\d+)/);
+        if (!m) continue;
+       
+        parseIngredient(line, parseInt(m[1])-1, i);
     }
 }
 
@@ -71,20 +59,19 @@ parse_formula();
 console.log('correct',correct,'error',error, 'rate',(correct-error)/correct)
 
 const gen_formula_ingredient=()=>{
-    const arr=fromObj(formula_ingredient,(a,b)=>b.join(','));
-//serial is one-base
-    arr.unshift('^:<name=formula keytype=serial preload=true caption=方劑>ingredients=keys:ingredient');
-    writeChanged('off/7-formula.tsv',arr.join('\n'),true);
+    //serial is one-base
+    ingredients.unshift('^:<name=ingredients keytype=serial preload=true caption=配方之藥>member=keys:ingredient');
+    writeChanged('off/7-ingredients.tsv',ingredients.join('\n'),true);
 }
 
 
 const fill_ingredient_formula=()=>{
-    const arr=fromObj(ingredient_formula,(a,b)=>a+'\t'+b.join(','));
+    const arr=fromObj(ingredient_members,(a,b)=>a+'\t'+b.join(','));
     arr.sort(alphabetically0);
-    arr.unshift('^:<name=ingredient2formula preload=true caption=藥到方>name:string\tformulas=numbers');
+    arr.unshift('^:<name=ingredient preload=true caption=藥之配方>name:string\tmember=numbers:ingredients');
     writeChanged('off/8-ingredient.tsv',arr.join('\n'),true);
   
 }
 
-gen_formula_ingredient();
-fill_ingredient_formula();
+gen_formula_ingredient(); //方 - 藥名清單
+fill_ingredient_formula(); //藥- 含此藥之方
